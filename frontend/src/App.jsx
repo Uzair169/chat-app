1003b2a3466d45105badee0de30553459990a8e1
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2024-09-18 19:56:44 +0500

    Signup functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
 import Home from './pages/home/home.jsx'
 import './index.css'
+import Login from './pages/login/login.jsx'
+import Signup from './pages/signup/signup.jsx'
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
+import { useAuthContext } from './Context/authContext.jsx'
+
 
 function App() {
+  const { authUser } = useAuthContext()
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-     <Home />
+      <Routes>
+        <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+        <Route path='/login' element={authUser ? <Navigate to="/" /> : <Login />} />
+        <Route path='/signup' element={authUser ? <Navigate to="/" /> : <Signup />} />
+      </Routes>
+      <Toaster />
     </div>
   )
 }
--- frontend/src/pages/signup/signup.jsx
+++ frontend/src/pages/signup/signup.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GenderCheckbox from './genderCheckbox.jsx'
+import { Link } from 'react-router-dom'
+import useSignup from '../../hooks/useSignup.js'
 
 function signup() {
+
+  const [input, setInputs] = useState({
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    gender: ''
+  })
+
+  const { loading, signup } = useSignup()
+  const handleCheckboxChange = (gender) => {
+    setInputs({ ...input, gender })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await signup(input)
+  }
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -10,18 +31,22 @@ function signup() {
           <span className='text-blue-500'>GhumXHum</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className='label p-2'>
               <span className='text-base label-text outline-base-300'>Full Name</span>
             </label>
-            <input type='text' placeholder='Uzair Imran' className='w-full input input-bordered h-10' />
+            <input type='text' placeholder='Uzair Imran' className='w-full input input-bordered h-10' value={setInputs.fullName}
+              onChange={(e) => setInputs({ ...input, fullName: e.target.value })}
+            />
           </div>
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Username</span>
             </label>
-            <input type='text' placeholder='uzairandali412' className='w-full input input-bordered h-10' />
+            <input type='text' placeholder='uzairandali412' className='w-full input input-bordered h-10'
+              value={input.username} onChange={(e) => setInputs({ ...input, username: e.target.value })}
+            />
           </div>
           <div>
             <label className='label'>
@@ -31,6 +56,8 @@ function signup() {
               type='password'
               placeholder='Enter password'
               className='w-full input input-bordered h-10'
+              value={input.password}
+              onChange={(e) => setInputs({ ...input, password: e.target.value })}
             />
           </div>
           <div>
@@ -41,17 +68,23 @@ function signup() {
               type='password'
               placeholder='Confirm password'
               className='w-full input input-bordered h-10'
+              value={input.confirmPassword}
+              onChange={(e) => setInputs({ ...input, confirmPassword: e.target.value })}
             />
           </div>
 
-          <GenderCheckbox />
+          <GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={input.gender} />
 
-          <a className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
+          <Link className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' to='/login'>
             Already have an account?
-          </a>
+          </Link>
 
           <div>
-            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+            <button className='btn btn-block btn-sm mt-2 border border-slate-700'
+              disabled={loading}
+            >
+              {loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+            </button>
           </div>
         </form>
       </div>
