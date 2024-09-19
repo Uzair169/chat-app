3c806150e8dc7d314e1ab4086dda8ad5a801d854
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2024-09-19 16:35:45 +0500

    Backend and Frontend connected

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Messages from './Messages.jsx'
 import MessageInput from './MessageInput.jsx'
 import { TiMessages } from "react-icons/ti"
+import useConversation from '../../zustand/useConversation.js'
 
 
 const MessageContainer = () => {
-    const noChatSelected = true
+    const { selectedConversation, setSelectedConversation } = useConversation()
+
+    //Function to cleanup, meaning that it will not revert back to the same conversation if i logout and login again.
+    useEffect(() => {
+
+        return () => setSelectedConversation(null)
+    }, [setSelectedConversation])
 
     return (
         <div className='md-min-w-[450px] flex flex-col'>
-            <>
-                {noChatSelected ? <NoChatSelected /> : (
+            {!selectedConversation ? <NoChatSelected /> : (
+                <>
+
                     <div className='bg-slate-500 px-4 py-2 mb-2'>
                         <span className='label-text'>To:</span> {" "}
-                        <span className='text-gray-900 font-bold'>John Doe</span>
-                    </div>)}
+                        <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+                    </div>
 
-                <Messages />
-                <MessageInput />
-            </>
+                    <Messages />
+                    <MessageInput />
+                </>
+            )}
         </div>
     )
 }
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiSearch } from "react-icons/ci";
-
+import useConversation from '../../zustand/useConversation';
+import useGetConversations from '../../hooks/useGetConversations';
+import toast from 'react-hot-toast';
 const SearchInput = () => {
+
+  const [search, setSearch] = useState("")
+  const { setSelectedConversation } = useConversation()
+  const { conversations } = useGetConversations()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!search) return
+    if (search.length < 3) {
+      return toast.error("Search must be at least 3 characters long")
+    }
+
+    const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()))
+
+    if (conversation) {
+      setSelectedConversation(conversation)
+      setSearch("")
+
+    } else {
+      toast.error('No such user found')
+    }
+
+  }
+
   return (
-    <form className='flex items-center gap-2'>
-        <input type="text" placeholder='Search' className='input input-bordered rounded-full' />
-        <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
-        <CiSearch className='w-6 h-6 '/>
-        </button>
+    <form onSubmit={handleSubmit} className='flex items-center gap-2'>
+      <input type="text" placeholder='Search' className='input input-bordered rounded-full' value={search} // bind the input field to the search state
+        onChange={(e) => setSearch(e.target.value)} />
+      <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+        <CiSearch className='w-6 h-6 ' />
+      </button>
     </form>
   )
 }
